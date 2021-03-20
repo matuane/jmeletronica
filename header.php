9ab3f281120be0bfb6c8e426ed3a04cc428f772b
Mateus Antuane <mateus_antuane@unifei.edu.br>
2021-03-19 22:14:22 -0300

    Estrutura da pagina montanda (sem WP)

--- header.php
+++ header.php
@@ -5,7 +5,9 @@
 	    <meta http-equiv="X-UA-Compatible" content="IE=edge">
 	    <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
+	    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
 	    <link rel="stylesheet" href="<?php echo get_stylesheet_directory_uri(); ?>/style.css">
+
 
 	    <title><?php bloginfo('name'); ?></title>
 	    <!-- Header Wordpress -->
@@ -14,12 +16,21 @@
 	</head>
 	<body>
 	    <header>
-	    	<nav>
-	    		<li>Inicio</li>
-	    		<li>Segurança</li>
-	    		<li>Telecomunicações</li>
-	    		<li>Informática</li>
-	    		<li>Contatos</li>
-	    	</nav>
-	    	<h1><img src="" alt=""></h1>
+	    		<div class="navbar navbar-light">
+
+	    			<div class="container-fluid">
+	    				<a class="navbar-brand" href="#">
+					      	<img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQHoVcTZpSTFA2e3vFVPNXlLrRQFGD0TvbzTw&usqp=CAU" alt="" width="100" height="100" class="d-inline-block align-top">
+					      	<?php bloginfo('name'); ?>
+					    </a>
+	    			</div>
+	    		
+		    		<div class="navbar_menu">
+		    			<nav>
+		    				<li>Menu</li>
+		    				<li>Menu</li>
+		    				<li>Menu</li>
+		    			</nav>
+		    		</div>
+	    		</div>
 	    </header>
--- page-home.php
+++ page-home.php
@@ -3,37 +3,38 @@
 ?>
 <?php get_header(); ?>
 
-	<section>
-		<div>
-			<img src="">
+	<section class="sobre_empresa">
+		<div class="sobre_image">
+			<img src="https://casa.abril.com.br/wp-content/uploads/2019/08/1-predio-de-apartamentos-em-sao-paulo-recebe-fachada-pixelada.png">
 		</div>
-		<div>
+		<div class="sobre_texto img-fluid">
+			<h3>Cras ultricies ligula sed magna dictum porta. Curabitur arcu erat, accumsan id imperdiet et, porttitor at sem. Nulla quis lorem ut libero mnec, egestas non nisi.Cras ultricies ligula sed magna dictum porta. Curabitur arcu erat, accumsan id imperdiet et, porttitor at sem. Nulla quis lorem ut libero mnec, egestas non nisi.Cras ultricies ligula sed magna dictum porta. Curabitur arcu erat, accumsan id imperdiet et, porttitor at sem. Nulla quis lorem ut libero mnec, egestas non nisi.Cras ultricies.</h3>
 			<h3>Cras ultricies ligula sed magna dictum porta. Curabitur arcu erat, accumsan id imperdiet et, porttitor at sem. Nulla quis lorem ut libero mnec, egestas non nisi.</h3>
 			<button>CONHEÇA NOSSOS PRODUTOS</button>
 		</div>
 	</section>
 
 	<section>
-		<div>
-			<h3>NOSSOS PRODUTOS</h3>
-			<hr>
+		<div class="titulo_do_produtos">
+			<h3 class="titulo_produtos">NOSSOS PRODUTOS</h3>
+			<hr class="hr_titulo_produtos">
 		</div>
-		<div>
-			<div>
+		<div class="secao_card_produtos">
+			<div class="card_produtos">
 				<h3>SEGURANÇA</h3>
-				<img src="">
+				<img src="https://i.pinimg.com/originals/b6/49/90/b64990688a617d274410359fa0fc8312.jpg">
 				<p>Curabitur arcu erat, accumsan id imperdiet et, porttitor at sem. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque in ipsum id orci porta dapibus.</p>
 				<button>QUERO SABER MAIS</button>
 			</div>
-			<div>
+			<div class="card_produtos">
 				<h3>TELECOMUNICAÇÕES</h3>
-				<img src="">
+				<img src="https://i.pinimg.com/564x/86/2f/d4/862fd4e1666a9931e470df2b84967955.jpg">
 				<p>Curabitur arcu erat, accumsan id imperdiet et, porttitor at sem. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque in ipsum id orci porta dapibus.</p>
 				<button>QUERO SABER MAIS</button>
 			</div>
-			<div>
+			<div class="card_produtos">
 				<h3>INFORMÁTICA</h3>
-				<img src="">
+				<img src="https://www.euromav.com.br/imagens/informacoes/manutencao-placa-eletronica-03.jpg">
 				<p>Curabitur arcu erat, accumsan id imperdiet et, porttitor at sem. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque in ipsum id orci porta dapibus.</p>
 				<button>QUERO SABER MAIS</button>
 			</div>
@@ -42,19 +43,22 @@
 
 	<section>
 
-		<div>
+		<div class="titulo_contatos">
 			<h3>CONTATOS</h3>
 			<hr>
 		</div>
 
-		<div>
-			<img src="">
-			<p>3431-3277</p>
+		<div class="coluna_contatos">
+			<div class="contatos">
+				<img src="">
+				<p>3431-3277</p>
+			</div>
+			<div class="contatos">
+				<img src="">
+				<p>+55 35 9 8897-3277</p>
+			</div>
 		</div>
-		<div>
-			<img src="">
-			<p>+55 35 9 8897-3277</p>
-		</div>
+
 	</section>
 
 <?php get_footer(); ?>
